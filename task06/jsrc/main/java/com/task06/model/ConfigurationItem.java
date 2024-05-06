e1aa36c9a1ca93e0cc3f5c14c130b55164cfeb8f
srishtidubeyyy <100397793+srishtidubeyyy@users.noreply.github.com>
2024-05-06 13:39:48 +0530

    add task06 file

---
+++ task06/jsrc/main/java/com/task06/model/ConfigurationItem.java
@@ -0,0 +1,24 @@
+
+package com.task06.model;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+@DynamoDBTable(tableName = "Configuration")
+public class ConfigurationItem {
+    private String key;
+    private int value;
+    @DynamoDBHashKey(attributeName = "Key")
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    @DynamoDBAttribute(attributeName = "Value")
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
