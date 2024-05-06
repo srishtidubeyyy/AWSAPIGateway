6a565eed668bc5cac30ff2fec9339105f643ef25
srishtidubeyyy <100397793+srishtidubeyyy@users.noreply.github.com>
2024-05-06 17:15:17 +0530

    updated files

---
+++ task06/AuditInsert.java
@@ -0,0 +1,83 @@
+package com.task06.models;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuditInsert {
+    private String id;
+    private String itemKey;
+    private LocalDateTime modificationTime;
+    private String updatedAttribute;
+    private Object newValue;
+
+    @DynamoDBHashKey
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDBAttribute
+    public String getItemKey() {
+        return itemKey;
+    }
+
+    public void setItemKey(String itemKey) {
+        this.itemKey = itemKey;
+    }
+
+    @DynamoDBAttribute
+    public LocalDateTime getModificationTime() {
+        return modificationTime;
+    }
+
+    public void setModificationTime(LocalDateTime modificationTime) {
+        this.modificationTime = modificationTime;
+    }
+
+    @DynamoDBAttribute
+    public String getUpdatedAttribute() {
+        return updatedAttribute;
+    }
+
+    public void setUpdatedAttribute(String updatedAttribute) {
+        this.updatedAttribute = updatedAttribute;
+    }
+
+    @DynamoDBAttribute
+    public Object getNewValue() {
+        return newValue;
+    }
+
+    public void setNewValue(Object newValue) {
+        this.newValue = newValue;
+    }
+
+    @Override
+    public String toString() {
+        return "AuditInsert{" +
+                "id='" + id + '\'' +
+                ", itemKey='" + itemKey + '\'' +
+                ", modificationTime=" + modificationTime +
+                ", updatedAttribute='" + updatedAttribute + '\'' +
+                ", newValue=" + newValue +
+                '}';
+    }
+
+    public Map<String, AttributeValue> toMap() {
+        Map<String, AttributeValue> map = new HashMap<>();
+        map.put("id", new AttributeValue().withS(id));
+        map.put("itemKey", new AttributeValue().withS(itemKey));
+        map.put("modificationTime", new AttributeValue().withS(modificationTime.toString()));
+        map.put("updatedAttribute", new AttributeValue().withS(updatedAttribute));
+        map.put("newValue", new AttributeValue().withN(String.valueOf(newValue)));
+        return map;
+    }
+}
