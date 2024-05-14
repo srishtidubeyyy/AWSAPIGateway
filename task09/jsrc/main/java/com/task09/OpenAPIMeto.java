8662fbb88c808c1467bf3d2f06ca2e7b7cae9df7
srishtidubeyyy <100397793+srishtidubeyyy@users.noreply.github.com>
2024-05-14 11:45:44 +0530

    task09

---
+++ task09/jsrc/main/java/com/task09/OpenAPIMeto.java
@@ -0,0 +1,28 @@
+package com.task09;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class OpenAPIMeto {
+    private static final String API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+
+    public String getWeatherForecast() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return response.body();
+        } else {
+            throw new IOException("Failed to fetch weather forecast: " + response.statusCode());
+        }
+    }
+
+
+}
